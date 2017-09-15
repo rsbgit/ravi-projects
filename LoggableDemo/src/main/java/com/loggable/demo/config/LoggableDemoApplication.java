aa1cf1d3fddcbabcdbe9eff4fa25d81849d9b9d4
rbhat <Jan@2017>
2017-09-15 10:03:32 +0530

    ProjectSetup

---
+++ LoggableDemo/src/main/java/com/loggable/demo/config/LoggableDemoApplication.java
@@ -0,0 +1,12 @@
+package com.loggable.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggableDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggableDemoApplication.class, args);
+	}
+}
